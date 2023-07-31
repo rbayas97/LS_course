c4dd53b73b6b61bedaf389f24c6eb9c105c434bb
Raul <76450597+rbayas97@users.noreply.github.com>
2023-07-31 10:05:49 -0400

    Added subtraction, multiplication, division

--- RB101/RB101-lesson2/calculator.rb
+++ RB101/RB101-lesson2/calculator.rb
@@ -16,6 +16,12 @@ operator = Kernel.gets().chomp()
 
 if operator == '1'
   result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+else 
+  result = number1.to_f() / number2.to_f()
 end 
 
 Kernel.puts("The result is #{result}")
