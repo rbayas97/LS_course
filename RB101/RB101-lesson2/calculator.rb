3d703984dcd7c4fe6d1f631bb08f77da3e319eb9
Raul <76450597+rbayas97@users.noreply.github.com>
2023-07-31 12:17:31 -0400

    Create input validation for operators with loop

--- RB101/RB101-lesson2/calculator.rb
+++ RB101/RB101-lesson2/calculator.rb
@@ -12,42 +12,59 @@ end
 
 prompt("Welcome to Calculator!")
 
-number1 = ''
 loop do 
-  prompt("What is the first number?")
-  number1 = Kernel.gets().chomp()
-  
-  if valid_number?(number1)
-    break 
-  else 
-    prompt ("Please enter a valid number")
+  number1 = ''
+  loop do 
+    prompt("What is the first number?")
+    number1 = Kernel.gets().chomp()
+    
+    if valid_number?(number1)
+      break 
+    else 
+      prompt ("Please enter a valid number")
+    end 
   end 
+
+  number2 = ''
+  loop do 
+    prompt("What is the second number?")
+    number2 = Kernel.gets().chomp()
+    
+    if valid_number?(number2)
+      break 
+    else
+      prompt("Please enter a valid number")
+    end 
+  end 
+
+  prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+  
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break 
+    else
+      prompt("Must choose 1, 2, 3 or 4")
+    end 
+  end
+
+  result = case operator
+          when '1'
+            number1.to_i + number2.to_i
+          when '2'
+            number1.to_i - number2.to_i
+          when '3'
+            number1.to_i * number2.to_i
+          when '4'
+            number1.to_f / number2.to_f
+  end
+
+  prompt("The result is #{result}")
+  prompt("Do you want to perform another calculation?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end 
 
-number2 = ''
-loop do 
-  prompt("What is the second number?")
-  number2 = Kernel.gets().chomp()
-  
-  if valid_number?(number2)
-    break 
-  else
-    prompt("Please enter a valid number")
-  end 
-end 
-
-prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-result = case operator
-         when '1'
-          number1.to_i + number2.to_i
-         when '2'
-          number1.to_i - number2.to_i
-         when '3'
-          number1.to_i * number2.to_i
-         when '4'
-          number1.to_f / number2.to_f
-end
-
-prompt("The result is #{result}")
+prompt("Thank you for using the calculator. Goodbye!")
