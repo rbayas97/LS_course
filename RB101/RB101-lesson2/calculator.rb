f6080c76271b1d6dc612ec0329baffe7821726e7
Raul <76450597+rbayas97@users.noreply.github.com>
2023-07-31 11:35:17 -0400

    Change 'if/else' to case statement assigned to result

--- RB101/RB101-lesson2/calculator.rb
+++ RB101/RB101-lesson2/calculator.rb
@@ -17,14 +17,15 @@ number2 = Kernel.gets().chomp()
 prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
 
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-else 
-  result = number1.to_f() / number2.to_f()
-end 
+result = case operator
+         when '1'
+          number1.to_i + number2.to_i
+         when '2'
+          number1.to_i - number2.to_i
+         when '3'
+          number1.to_i * number2.to_i
+         when '4'
+          number1.to_f / number2.to_f
+end
 
 prompt("The result is #{result}")
