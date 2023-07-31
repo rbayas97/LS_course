caa65fc56ff7bfc80d6420461dec8bd815505132
Raul <76450597+rbayas97@users.noreply.github.com>
2023-07-31 11:30:45 -0400

    Add prompt method

--- RB101/RB101-lesson2/calculator.rb
+++ RB101/RB101-lesson2/calculator.rb
@@ -2,16 +2,19 @@
 # ask the user for an operation to perform on the two numbers 
 # perform the operation on the two numbers
 # output the result 
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end 
 
-Kernel.puts("Welcome to Calculator!")
+prompt("Welcome to Calculator!")
 
-Kernel.puts("What is the first number?")
+prompt("What is the first number?")
 number1 = Kernel.gets().chomp()
 
-Kernel.puts("What is the second number?")
+prompt("What is the second number?")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
 
 if operator == '1'
@@ -24,4 +27,4 @@ else
   result = number1.to_f() / number2.to_f()
 end 
 
-Kernel.puts("The result is #{result}")
+prompt("The result is #{result}")
