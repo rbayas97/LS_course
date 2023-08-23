b172df4511f2ecaf7df5c2c3301bcfb5db04046a
Raul <76450597+rbayas97@users.noreply.github.com>
2023-08-23 14:39:41 -0400

    PEDAC process for multiple_of_3_and_5

--- RB101-119_Easy/Easy_4/multiples_of_3_and_5.rb
+++ RB101-119_Easy/Easy_4/multiples_of_3_and_5.rb
@@ -1,19 +1,27 @@
-# Input: Integers
-# Output: Sum of multiples (3 or 5) that lie between 1 and other number
+# Problem: Given a number create a method that seaarches for all multiples of 3 and 5
+#          that lie between 1 and given number. Computer the sum of those multiples
 
-def multisum(number)
-  sum_numbers = []
-  while number % 5 > 0 
-    number = number / 5 
-    sum_numbers << number
-  end 
-
-  while number % 3 > 0 
-    number = number / 3
-    sum_numbers << number
-  end 
-  sum_numbers.sum
-end 
+# Input: Number(integer)
+# Output: sum of all multiples of 3 or 5 that lie between 1 and input number
 
 
-p multisum(5)
+# Rules:
+#      Explicit Requirments: 
+#        - Returns sum of all multiples 3 or 5 
+#        - Number passed in as an argument is greater than 1
+#      Implicit Requirements: 
+#        -  Multiple of both 3 and 5 only count once.
+#        -  Can include argument as multiple if applicable. 
+
+
+# Data Structure/ Algorithm 
+#      - Create an multiples_array that will hold multiples of 3 or 5
+#      - Create two `multiple` variables 3 and 5
+#      - Create loop that will go up to said argument
+#         - if `multiple` variables is less than argument add to multiples_array
+#         - add 3 and 5 respectively to `multiples` variable
+#         - start with 3 first than 5
+#         - if number is already included in `multiples_array` skip to next
+#      - add up the multiples_array total
+#      - return the sum
+
