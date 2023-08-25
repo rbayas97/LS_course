551392c41bfceac13685c08ae2600d62dabcbe29
Raul <76450597+rbayas97@users.noreply.github.com>
2023-08-25 15:25:58 -0400

    Completed problems 3-6

--- RB110/lesson1/practice_problems.rb
+++ RB110/lesson1/practice_problems.rb
@@ -2,7 +2,6 @@
 #    posiitons in array 
 
 # flinstones = ['Fred', 'Barney', 'Wilma', 'Betty', 'Pebbles', 'BamBam']
-
 # flinstones_hash = {}
 
 # flinstones.each_with_index do |name, index|
@@ -23,4 +22,50 @@
 # end 
 
 # p combined_age
+
+
+
+
+## 3. Remove people with age 100 and greater from the hash
+# ages = { 'Herman'=> 32, 'Lily' => 30, 'Grandpa' => 402, 'Eddie' => 10 }
+# ages.delete_if { |_, value| value >= 100}
+# Actual solution: ages.keep_if { |_, age| age < 100 }
+
+
+
+
+## 4. Pick out the minimum age from our current Munster family hash
+# ages = { "Herman" => 32, "Lily" => 30, "Brandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+# pages.min_by { |key, value| value }
+
+
+
+
+
+## 5. Find the index of the first name that starts with 'Be'
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# p flintstones.index { |name| name[0,2] == 'Be' }
+
+
+
+
+
+## 6. Write code that changes te arrays so that all names are shorteded to first 3
+#     characters. Do not create a new array
+
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+# flintstones.map! { |value| value[0,3] }
+# p flintstones
+
+
+
+
+
+## 7. Create a hash that expresses the frequency with which each letter occurs in string
+statement = 'The Flintstones Rock'
+
+
+
+
+
 
