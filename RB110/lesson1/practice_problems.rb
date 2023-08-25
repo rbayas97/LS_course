19af092875693bc1b0f7fc8e32a96a995c646aa0
Raul <76450597+rbayas97@users.noreply.github.com>
2023-08-25 15:07:31 -0400

    completed first 2 problems

---
+++ RB110/lesson1/practice_problems.rb
@@ -0,0 +1,26 @@
+## 1. Given array turn array into hash where names are keys and values are 
+#    posiitons in array 
+
+# flinstones = ['Fred', 'Barney', 'Wilma', 'Betty', 'Pebbles', 'BamBam']
+
+# flinstones_hash = {}
+
+# flinstones.each_with_index do |name, index|
+#   flinstones_hash[name] = index 
+# end 
+
+# p flinstones_hash
+
+
+
+
+## 2. Add up all of the ages from the Munster family hash:
+# ages = { 'Herman' => 32, "Lily" => 30, 'Grandpa' => 5843, 'Eddie' => 10, 'Marilyn' => 22, 'Spot' => 237 }
+
+# combined_age = 0
+# ages.each do |key, value|
+#   combined_age += value
+# end 
+
+# p combined_age
+
