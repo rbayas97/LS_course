ac9022f3fa74a12ebc69126d015eb033ed23fe51
Raul <76450597+rbayas97@users.noreply.github.com>
2023-08-25 18:12:30 -0400

    Completed problems 7-10

--- RB110/lesson1/practice_problems.rb
+++ RB110/lesson1/practice_problems.rb
@@ -62,10 +62,49 @@
 
 
 ## 7. Create a hash that expresses the frequency with which each letter occurs in string
-statement = 'The Flintstones Rock'
+# statement = 'Hello'
+# frequency_hash = {}
+# statement_array = statement.delete(' ').split('')
+# statement_array.each do |character| 
+#   frequency_hash[character] = statement_array.count(character)
+# end 
+
+# p frequency_hash
 
 
 
 
 
+## 9. Write you own version of the rails `titleize` method implementation
+# words = 'the flinstones rock'
+# words = words.split.map { |word| word.capitalize }.join(' ')
+# p words
+
+
+
+
+## 10. Given munsters hash modify hash such that each member has additional 'age_group key'
+#      that has one of three values describing the age group of the family member.
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.map do |key, value|
+  age = value['age']
+  case age
+  when 0..17
+    value['age_group'] = 'kid'
+  when 18..64
+    value['age_group'] = 'adult'
+  else 
+    value['age_group'] = 'senior'
+  end
+end 
+
+p munsters
 
