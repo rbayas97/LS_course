dd4a35afeb34bcc7a1c7d41dbcfdc0ca1ccd7cb4
Raul <76450597+rbayas97@users.noreply.github.com>
2023-08-27 19:58:59 -0400

    PEDAC for convert_string_number.rb

---
+++ RB101-119_Easy/Easy_4/convert_string_number.rb
@@ -0,0 +1,12 @@
+# Problem: Given a string of digits, return the appropriate number as an integer.
+#          Do not use any methods to convert from string to integer to achieve this.
+
+
+# Input: String of a Number
+# Output: Number of string as an Integer 
+
+# Requirements: 
+#       - Explicit:
+#           - Can NOT use any methods that convert from string to integer
+#           - Do not have to worry about invalid characters. Characters will be numeric
+#           - Return an integer type 
