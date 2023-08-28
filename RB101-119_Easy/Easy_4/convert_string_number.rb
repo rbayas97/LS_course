7160b22d2b51d5674b5dd354d38019e490013f16
Raul <76450597+rbayas97@users.noreply.github.com>
2023-08-27 20:34:33 -0400

    implement solution for convert_string_number.rb

--- RB101-119_Easy/Easy_4/convert_string_number.rb
+++ RB101-119_Easy/Easy_4/convert_string_number.rb
@@ -3,10 +3,31 @@
 
 
 # Input: String of a Number
-# Output: Number of string as an Integer 
+# Output: String input as an Integer 
 
 # Requirements: 
 #       - Explicit:
 #           - Can NOT use any methods that convert from string to integer
 #           - Do not have to worry about invalid characters. Characters will be numeric
 #           - Return an integer type 
+#       - Implicit: 
+#           - None for this question
+
+# Data Structure/ Algorithm: 
+#       -  split up the string to get each indivdual string number
+#       - get the ordinance number of each string
+#       - the return value will give as an indication of where that number is represented
+#       - add or subtract from the return value in last step to get integer value 
+#       - put the integer value into an array
+#       - join the array together 
+#       - return the joined array value
+
+
+# Code 
+
+def string_to_integer(num_string)
+  num_integer = num_string.chars.map { |number| number.ord - 48 }
+  p num_integer.inject{ |a, i| p a * 10 }
+end 
+
+string_to_integer('123')
