59619871ac0c7e75c57d925504906c5d293991aa
Raul <76450597+rbayas97@users.noreply.github.com>
2023-09-01 14:20:08 -0400

    Complete code step for ascii_string_value.

--- RB101-119_Easy/Easy_5/ascii_string_value.rb
+++ RB101-119_Easy/Easy_5/ascii_string_value.rb
@@ -28,3 +28,16 @@
 
 
 
+def ascii_value(str)
+  sum = 0
+  str.split('').each do |char|
+    sum += char.ord
+  end
+
+  sum
+end 
+
+p ascii_value('Four score') == 984
+p ascii_value('Launch School') == 1251
+p ascii_value('a') == 97
+p ascii_value('') == 0
