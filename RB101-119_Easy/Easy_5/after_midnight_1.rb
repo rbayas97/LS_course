3ce6c09560615814e47ce29539f9dda6957cacd3
Raul <76450597+rbayas97@users.noreply.github.com>
2023-09-02 21:33:51 -0400

    Work on PEDAC for after_midnight

---
+++ RB101-119_Easy/Easy_5/after_midnight_1.rb
@@ -0,0 +1,22 @@
+# Problem: Time of day can be represented as a number before or after midnight. Positive
+#          time is after midnight, negative before midnight. Write a method that take a 
+#          time using this minute format and return time in 24 hour format (hh:mm)
+
+
+# Input:  Integer, positive or negative
+# Output: String with time of input in 24 hour format (hh:mm)
+
+
+# Requirements:
+#      - Explicit: 
+#             - Can NOT use ruby Date and Time classes
+#             - Method should work with any input integer
+#             - Must return a string in 24 hour format (hh:mm)
+#             - Disregard daylight savings and standard time and other complications
+#             - 0 should return "00:00"
+#      - Implicit
+#             - None for this problem 
+
+
+
+# Data Structure/ Algorithm
