c6a157ac4ff048d9fd163a61026a639414dc5d30
Raul <76450597+rbayas97@users.noreply.github.com>
2023-09-07 21:24:38 -0400

    Implement code solution for list_of_digits

--- RB101-119_Easy/Easy_5/list_of_digits.rb
+++ RB101-119_Easy/Easy_5/list_of_digits.rb
@@ -17,9 +17,29 @@
 # Data structure/Algorithm
 #       - Create an empty array and assign it to a variable 'digits'
 #       - Convert the input integer into a string so that the number is now a string
+#         Assign it to a variable int_string
 #       - Use the split method to seperate each character of the string into an array.
 #         Assign this method return value to string_digits
 #       - Loop through the string_digits array
 #            - For each string number use the to_i method to convert it back to an integer
 #            - Use the return  value of to_i and push that value to 'digits'
 #       - returns 'digits'
+
+
+
+def digit_list(int)
+  digits = []
+  int_string = int.to_s
+
+  arr_string_digits = int_string.chars
+  arr_string_digits.each do |str_num|
+    digits << str_num.to_i
+  end
+  
+  digits
+end 
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
