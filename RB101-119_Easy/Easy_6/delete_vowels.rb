a5670fb855c894252c2abc20a74a6814bed046ab
Raul <76450597+rbayas97@users.noreply.github.com>
2023-09-09 13:12:37 -0400

    Implement PEDAC process for delete_vowels.rb

---
+++ RB101-119_Easy/Easy_6/delete_vowels.rb
@@ -0,0 +1,24 @@
+# Problem: Write a method that takes an array of strings and returns an array of the 
+#          same string values without the vowels
+
+
+# Input:  Array of Strings
+# Output: Array of Strings with the vowels removed 
+
+
+# Requirements:
+#      - Explicit: 
+#          - Return an array of strings with the vowels removed from each string
+#          - Returned array should have same number of elements as the input array
+#          - String format should stay the same. Capitalization 
+#      - Implicit: 
+#          - If string is all vowels, should return an empty string
+
+
+# Data Structure/ Algorihm
+#      - Create a variable `vowels_removed` and set it to an empty array
+#      - Loop through the array, and for each string loop through each character
+#           - if the character is a vowel append it to the array
+#           - if not append an empty space to the array
+#      - return the vowels_removed array 
+
