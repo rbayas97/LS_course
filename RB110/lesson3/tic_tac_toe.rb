f85799356f94927d980a95a0123f3af2fe20a150
Raul <76450597+rbayas97@users.noreply.github.com>
2023-09-10 15:09:05 -0400

    Implement loop to play tic-tac-toe unless player inputs no

--- RB110/lesson3/tic_tac_toe.rb
+++ RB110/lesson3/tic_tac_toe.rb
@@ -21,6 +21,7 @@ end
 
 def display_board(brd)
   system 'clear'
+  puts "You are a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -90,22 +91,30 @@ def detect_winner(brd)
   nil
 end 
 
-board = initialize_board
+loop do 
+  board = initialize_board
 
-loop do
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
   display_board(board)
 
-  player_places_piece!(board)
-  break if someone_won?(board) || board_full?(board)
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else 
+    prompt "It's a tie!"
+  end 
 
-  computer_places_piece!(board)
-  break if someone_won?(board) || board_full?(board)
-end
-
-display_board(board)
-
-if someone_won?(board)
-  prompt "#{detect_winner(board)} won!"
-else 
-  prompt "It's a tie!"
+  prompt "Play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end 
+
+prompt "Thanks for playing Tic Tac Toe. Good bye!"
