c6d836be28437d99d0b34da276ba457da2f866bb
Raul <76450597+rbayas97@users.noreply.github.com>
2023-09-16 21:19:44 -0400

    Implement PEDAC process for does_my_list_include?

---
+++ RB101-119_Easy/Easy_6/does_my_list_include?.rb
@@ -0,0 +1,27 @@
+# Problem: Write a method named include? that takes two arguments (array, and value).
+#          Method should return a boolean value based on whethere the argument value 
+#          is included in the array.
+
+# Inputs: Array, and integer
+# Output: True or False 
+
+# Requirements:
+#       - Explicit: 
+#            - Do NOT use Array#include? method in your solution.
+#            - Return value should be a true or false value based on whether the input
+#              value is included in the array argument.
+#       - Implicit:
+#            - nil can be used as an input value. Should return true if the array contains
+#              nil and false if it doesn't
+#            - empty array should return false if there is any input value in the second
+#              argument.
+
+# Data Structure/ Algorithm
+#       - Use the any? method to check whether any of the elements in the array
+#         argument are equal to the input integer.
+#       - The any? method will return a boolean value, true or false
+#           - This method will then act as if whether the input integer is in the 
+#             input array. Are any of the elements in the array equal to the input?
+#           - True means that it is included, false means it is not.
+#
+
