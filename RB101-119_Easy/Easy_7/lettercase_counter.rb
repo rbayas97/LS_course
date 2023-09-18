65b62060ab3678ac973d3809eec1c0ebaafa601c
Raul <76450597+rbayas97@users.noreply.github.com>
2023-09-18 15:02:08 -0400

    Implement code solution for lettercase_counter

--- RB101-119_Easy/Easy_7/lettercase_counter.rb
+++ RB101-119_Easy/Easy_7/lettercase_counter.rb
@@ -30,3 +30,20 @@
 #        to get the value of neither.
 #         - Use this return value as the key. Set it with letter_counter[] 
 
+
+def letter_case_count(string)
+  letter_case_count_hash = {}
+
+  letter_case_count_hash[:lowercase] = string.count('a-z')
+  letter_case_count_hash[:uppercase] = string.count('A-Z')
+  letter_case_count_hash[:neither] = string.length - letter_case_count_hash[:lowercase] - letter_case_count_hash[:uppercase]
+
+  letter_case_count_hash
+end 
+
+
+p letter_case_count('abCdef 123')  == { lowercase: 5, uppercase: 1, neither: 4 }
+p letter_case_count('AbCd +Ef')  == { lowercase: 3, uppercase: 3, neither: 2 }
+p letter_case_count('123')  == { lowercase: 0, uppercase: 0, neither: 3 }
+p letter_case_count('')  == { lowercase: 0, uppercase: 0, neither: 0 }
+
