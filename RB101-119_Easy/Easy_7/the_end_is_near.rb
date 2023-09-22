a2685bc21872042643ab6dbae5e9495e67852f91
Raul <76450597+rbayas97@users.noreply.github.com>
2023-09-22 14:47:25 -0400

    Implement PEDAC process for the_end_is_near

---
+++ RB101-119_Easy/Easy_7/the_end_is_near.rb
@@ -0,0 +1,18 @@
+# Problem: Write a method that return the next to last word of a string argument
+
+# Input:  String
+# Output: Second to last word in string
+
+# Requirements
+#    - Explicit: 
+#       - Words are any sequence of non-black characters
+#       - Input string will always contain at least 2 words
+#       - return the second to last word in the string
+#    - Implicit:
+#       - None for this problem
+
+
+# Data Structure/ Algorithm
+#    - Take input string and break it up with split, which will give us array of strings
+#      with each element being sepereated by the spaces in the string
+#    - Return the second to last element in the string with array referencing
