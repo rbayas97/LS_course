d9af75351b732212baa3a0ec6057825a724b6f59
Raul <76450597+rbayas97@users.noreply.github.com>
2023-10-05 16:49:08 -0400

    Refactor computer_places_piece! AI logic to seperate method

--- RB110/lesson3/tic_tac_toe.rb
+++ RB110/lesson3/tic_tac_toe.rb
@@ -63,22 +63,21 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = nil
+  square = determine_computer_move(brd)
+  brd[square] = COMPUTER_MARKER
+end
 
+def determine_computer_move(brd)
   potential_threat = WINNING_LINES.select do |line|
                      line.count {|number| brd[number] == PLAYER_MARKER} == 2 && 
                      line.count { |number| brd[number] == COMPUTER_MARKER} != 1
                      end
-  if potential_threat.length >= 1 
-    p potential_threat
-    square = potential_threat[0].select { |num| brd[num] != PLAYER_MARKER}[0]
-
+  if potential_threat.length >= 1
+    potential_threat[0].select { |num| brd[num] != PLAYER_MARKER }[0]
   else 
-    square = empty_squares(brd).sample 
+    empty_squares(brd).sample
   end 
-
-  brd[square] = COMPUTER_MARKER
-end
+end 
 
 def board_full?(brd)
   empty_squares(brd).empty?
