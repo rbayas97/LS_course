2fcab6230809395f0c27eb5e7db3b817dce728b1
Raul <76450597+rbayas97@users.noreply.github.com>
2023-10-09 21:44:48 -0400

    Implement high level psuedo code for twenty_one game

--- RB110/lesson3/twenty_one_files/twenty_one.rb
+++ RB110/lesson3/twenty_one_files/twenty_one.rb
@@ -1,12 +1,18 @@
-def display_card(num)
-  spaces = num.to_s.length == 1 ? 4 : 3
-  puts '+---------+'
-  puts '|         |'
-  puts '|         |'
-  puts "|    #{num}" + ' ' * spaces + '|'
-  puts '|         |'
-  puts '|         |'
-  puts '+---------+'
+DECK = { 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 
+         9 => 9, 10 => 10, 'Jack' => 'J', 'Queen' => 'Q', 'King' => 'K',
+         'Ace' => 'A'}
+
+
+def display_card(num1, num2)
+  spaces = num1.to_s.length == 1 ? 4 : 3
+  spaces_2 = num2.to_s.length == 1 ? 4 : 3
+  puts '+---------+'                       + '' +  '+---------+'  
+  puts '|         |'                       + '' +  '|         |'
+  puts '|         |'                       + '' +  '|         |'
+  puts "|    #{num1}" + ' ' * spaces + '|' + '' +  "|    #{num2}" + ' ' * spaces_2 + '|'
+  puts '|         |'                       + '' +  '|         |'
+  puts '|         |'                       + '' +  '|         |'
+  puts '+---------+'                       + '' +  '+---------+'  
 end 
 
-print display_card('K'), display_card(10)
+display_card(DECK[2], DECK[10])
