5e9231ee04091f897a9c1f1ec9bfde6e2e2f5188
Raul <76450597+rbayas97@users.noreply.github.com>
2023-10-10 14:34:44 -0400

    Refactor display_card for console output

--- RB110/lesson3/twenty_one_files/twenty_one.rb
+++ RB110/lesson3/twenty_one_files/twenty_one.rb
@@ -1,18 +1,38 @@
-DECK = { 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 
-         9 => 9, 10 => 10, 'Jack' => 'J', 'Queen' => 'Q', 'King' => 'K',
-         'Ace' => 'A'}
+# 1. Initialize deck
+# 2. Deal cards to player and dealer
+# 3. Player: hit or stay
+#    - Repeat until bust or 'stay'
+# 4. If player bust, dealer wins.
+# 5. Dealer turn: hit or stay
+#    - Repeat until total >= 17
+# 6. If dealer bust, player wins.
+# 7. Compare cards and declare winner.
+
+
+
+DECK_VALUES = { 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 
+         9 => 9, 10 => 10, 'J' => 'J', 'Q' => 'Q', 'K' => 'K',
+         'A' => 'A'}
 
 
 def display_card(num1, num2)
-  spaces = num1.to_s.length == 1 ? 4 : 3
-  spaces_2 = num2.to_s.length == 1 ? 4 : 3
-  puts '+---------+'                       + '' +  '+---------+'  
-  puts '|         |'                       + '' +  '|         |'
-  puts '|         |'                       + '' +  '|         |'
-  puts "|    #{num1}" + ' ' * spaces + '|' + '' +  "|    #{num2}" + ' ' * spaces_2 + '|'
-  puts '|         |'                       + '' +  '|         |'
-  puts '|         |'                       + '' +  '|         |'
-  puts '+---------+'                       + '' +  '+---------+'  
-end 
+  spaces = num1.to_s.length == 1 ? 3 : 2
+  spaces_2 = num2.to_s.length == 1 ? 3 : 2
+  puts '+--------+'                       + ' ' +  '+--------+'  
+  puts '|        |'                       + ' ' +  '|        |'
+  puts '|        |'                       + ' ' +  '|        |'
+  puts "|    #{num1}" + ' ' * spaces + '|' + ' ' +  "|    #{num2}" + ' ' * spaces_2 + '|'
+  puts '|        |'                       + ' ' +  '|        |'
+  puts '|        |'                       + ' ' +  '|        |'
+  puts '+--------+'                       + ' ' +  '+--------+'  
+end
 
-display_card(DECK[2], DECK[10])
+def initialize_deck
+  deck_count = {}
+  DECK_VALUES.each { |card, value| deck_count[card] = [value, 4] }
+  deck_count 
+end
+
+playing_deck = initialize_deck
+display_card(playing_deck.keys.sample, playing_deck.keys.sample)
+
