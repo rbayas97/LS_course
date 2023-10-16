c90cc2061f82988c7ba3e778adddb46113ef47ac
Raul <76450597+rbayas97@users.noreply.github.com>
2023-10-15 21:58:37 -0400

    Refactor code spacing format based off rubocop for twenty_one.rb

--- RB110/lesson3/twenty_one_files/twenty_one.rb
+++ RB110/lesson3/twenty_one_files/twenty_one.rb
@@ -8,67 +8,61 @@
 # 6. If dealer bust, player wins.
 # 7. Compare cards and declare winner.
 
-
-
-DECK_VALUES = { 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 
-         9 => 9, 10 => 10, 'J' => 10, 'Q' => 10, 'K' => 10,
-         'A' => 11}
+DECK_VALUES = { 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8,
+                9 => 9, 10 => 10, 'J' => 10, 'Q' => 10, 'K' => 10,
+                'A' => 11 }
 
 def prompt(message)
   puts "=> #{message}"
-end 
+end
 
 def display_card(card1, card2)
   spaces = card1.to_s.length == 1 ? 3 : 2
   spaces_2 = card2.to_s.length == 1 ? 3 : 2
-  puts '+-------+'                        + ' ' +  '+-------+'  
+  puts '+-------+'                        + ' ' +  '+-------+'
   puts '|       |'                        + ' ' +  '|       |'
   puts "|   #{card1}" + ' ' * spaces +'|' + ' ' +  "|   #{card2}" + ' ' * spaces_2 + '|'
   puts '|       |'                        + ' ' +  '|       |'                       
-  puts '+-------+'                        + ' ' +  '+-------+'  
+  puts '+-------+'                        + ' ' +  '+-------+'
 end
 
 def initialize_deck
   deck_count = {}
   DECK_VALUES.each { |card, value| deck_count[card] = [value, 4] }
-  deck_count 
+  deck_count
 end
 
 def draw_initial_hand!(deck)
   hand = []
   card = nil
-  2.times do 
+  2.times do
     card = deck.keys.sample
-    deck.map { |key, value| deck[key][1] -= 1 if key == card }
+    deck.map { |key, _| deck[key][1] -= 1 if key == card }
     hand << card
-  end 
-  hand 
+  end
+  hand
 end
 
 def draw_next_card!(deck, player_cards)
   card = deck.keys.sample
   player_cards << card
-  deck.map { |key, value| deck[key][1] -= 1 if key == card }
-  deck.delete_if{ |_, value| value[1] == 0 }
+  deck.map { |key, _| deck[key][1] -= 1 if key == card }
+  deck.delete_if { |_, value| value[1] == 0 }
 end
 
 def determine_ace_value(total, ace)
-  if total + DECK_VALUES[ace] <= 21
-    ace = 11
-  else 
-    ace = 1
-  end 
-end 
-    
+  total + DECK_VALUES[ace] <= 21 ? 11 : 1
+end
+
 def calculate_hand(player_cards)
   total = 0
   player_cards.each do |card|
     if card == 'A'
       ace_value = determine_ace_value(total, card)
       total += ace_value
-    else 
+    else
       total += DECK_VALUES[card]
-    end 
+    end
   end
   total
 end
@@ -77,12 +71,12 @@ def ask_to_hit_or_stay(total)
   prompt("Your hand is #{total}")
   prompt("Hit or Stay?")
   gets.chomp.downcase
-end 
+end
 
 def ask_to_play_again
   prompt("Play again? (y or n)")
-  answer = gets.chomp
-end 
+  gets.chomp
+end
 
 def determine_winner(player_hand_total, computer_hand_total)
   player_difference = 21 - player_hand_total
@@ -94,12 +88,12 @@ def determine_winner(player_hand_total, computer_hand_total)
     prompt("Your hand: #{player_hand_total}, Dealer hand: #{computer_hand_total}")
     prompt("You lost!")
   else
-    prompt("Your hand: #{player_hand_total}, Dealer hand: #{computer_hand_total}" )
+    prompt("Your hand: #{player_hand_total}, Dealer hand: #{computer_hand_total}")
     prompt("It's a draw!")
-  end 
-end 
+  end
+end
 
-loop do 
+loop do
   playing_deck = initialize_deck
   user_cards = draw_initial_hand!(playing_deck)
   dealer_cards = draw_initial_hand!(playing_deck)
@@ -121,10 +115,10 @@ loop do
         draw_next_card!(playing_deck, user_cards)
       elsif answer == 'stay'
         break
-      else 
+      else
         prompt('Wrong choice. Please enter Hit or Stay?')
-      end 
-    else 
+      end
+    else
       prompt('BUST! You went over 21!')
       bust = true
       break
@@ -143,15 +137,15 @@ loop do
     dealer_hand_total = calculate_hand(dealer_cards)
     if dealer_hand_total >= 17 && dealer_hand_total <= 21
       prompt('The dealer decides to stay. Time to reveal who won.')
-      break 
+      break
     elsif dealer_hand_total > 21
       bust = true
       prompt('The dealer bust! You win!')
       break
     else
-      prompt('The dealer is going to draw another card.' )
+      prompt('The dealer is going to draw another card.')
       draw_next_card!(playing_deck, dealer_cards)
-    end 
+    end
   end
 
   if bust == true
@@ -164,6 +158,4 @@ loop do
 
   play_again_response = ask_to_play_again
   break unless play_again_response.downcase.start_with?('y')
-end 
-
-
+end
